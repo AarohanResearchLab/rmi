87ed6d038c3bccfd11e88dc36a169d91c986689d
Aarohan Research Lab Software Pvt. Ltd <aarohanlab@gmail.com>
2021-06-25 12:06:06 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -227,3 +227,37 @@ function myFunction2() {
         moreText.style.display = "inline";
     }
 }
+
+// <M\Modal
+$(document).ready(function() {
+    // Watch More Link click handlers
+    const $popup = $('.video-popup');
+    const $modal = $('#modal');
+    const $closeIcon = $('.close');
+    const $watchMoreLink = $('.watch-more');
+
+    $watchMoreLink.click(function(e) {
+        $popup.fadeIn(200);
+        $modal.fadeIn(200);
+        debugger;
+        e.preventDefault();
+        var videoUrl = $(this).attr('data-video-url');
+        $('#iframeYoutube').attr('src', videoUrl)
+    });
+    $closeIcon.click(function() {
+        $popup.fadeOut(200);
+        $modal.fadeOut(200);
+    });
+    // for escape key
+    $(document).on('keyup', function(e) {
+        if (e.key === "Escape") {
+            $popup.fadeOut(200);
+            $modal.fadeOut(200);
+        }
+    });
+    // click outside of the popup, close it
+    $modal.on('click', function(e) {
+        $popup.fadeOut(200);
+        $modal.fadeOut(200);
+    });
+});
